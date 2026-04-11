72039b744392273a315e56033bd2b6330f35e129
Ciberaccion <argranados@yahoo.com>
2026-04-10 19:47:07 -0600

    first commit

---
+++ src/test/java/com/ciberaccion/notificacionservice/NotificacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberaccion.notificacionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
